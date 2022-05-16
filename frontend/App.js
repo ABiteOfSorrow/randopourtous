57bb2da5202fa1e45873590de245ce52dc585031
OliverPtps <oliver.printemps@yahoo.fr>
2022-05-16 18:21:46 +0200

    visuel create

--- frontend/App.js
+++ frontend/App.js
@@ -1,31 +1,35 @@
 // import { LogBox } from "react-native";
 // LogBox.ignoreAllLogs(true);
-import React from 'react'
-import {StyleSheet} from 'react-native'
-import {NativeBaseProvider} from 'native-base'
-import {NavigationContainer} from '@react-navigation/native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import {createStackNavigator} from '@react-navigation/stack'
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-import {FontAwesome5} from '@expo/vector-icons'
-
-import PresentScreen from "./screens/PresentScreen";
-import SignInScreen from "./screens/SignIn";
-import SignUpScreen from "./screens/SignUp";
-import HomeScreen from "./screens/Home";
+import PresentScreen from './screens/PresentScreen'
+import SignInScreen from './screens/SignIn'
+import SignUpScreen from './screens/SignUp'
+import HomeScreen from './screens/Home'
 import CreateScreen from './screens/Create'
-import SearchScreen from "./screens/Search";
+import SearchScreen from './screens/Search'
 // import MapScreen from './screens/Map'
 import ListScreen from './screens/List'
 import DetailScreen from './screens/Detail'
 import MyprofileScreen from './screens/MyProfile'
 // import OtherprofileScreen from './screens/Otherprofile'
-// import HistoryScreen from './screens/History'
+import HistoryScreen from "./screens/History";
 // import ListFriend from './screens/Friend'
 // import ChatScreen from './screens/Chat'
 // import ResumeScreen from './screens/Resume'
-import HamburgerMenuScreen from './screens/HamburgerMenu'
-//const store = configureStore({ reducer: combineReducers({  }) });
+import HamburgerMenuScreen from "./screens/HamburgerMenu";
+
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
+import user from "./reducers/user.reducer"
+const store = configureStore({ reducer: combineReducers({ user }) });
+import { Provider } from 'react-redux';
 
 const Tab = createBottomTabNavigator()
 
@@ -63,12 +67,12 @@ const BottomMenuTabs = () => {
           ...styles.shadow,
         },
       }}
-      initialRouteName="Home"
-    >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="List" component={ListScreen} />    
+      initialRouteName='Home'>
+
+      <Tab.Screen name="Home" options={{ gestureEnabled: false }} component={HomeScreen} />
+      <Tab.Screen name="List" component={ListScreen} />
       <Tab.Screen name="Search" component={DetailScreen} />
-      <Tab.Screen name="Profile" component={MyprofileScreen} />
+      <Tab.Screen name="Profile" component={HistoryScreen} />
     </Tab.Navigator>
   )
 }
@@ -76,40 +80,40 @@ const BottomMenuTabs = () => {
 const Stack = createStackNavigator()
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{headerShown: false}}
-          initialRouteName={''}>
-          <Stack.Screen name='Present' component={PresentScreen} />
-          <Stack.Screen name='SignIn' component={SignInScreen} />
-          <Stack.Screen name='SignUp' component={SignUpScreen} />
-          <Stack.Screen name='Home' component={BottomMenuTabs} />
-          <Stack.Screen name='Detail' component={DetailScreen} />
-          {/* <Stack.Screen name='Create' component={CreateScreen} />
+    <Provider store={store}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={""}>
+            <Stack.Screen name="Present" component={PresentScreen} />
+            <Stack.Screen options={{ gestureEnabled: false }} name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" options={{ gestureEnabled: false }} component={SignUpScreen} />
+            <Stack.Screen name="Home" options={{ gestureEnabled: false }} component={BottomMenuTabs} />
+            <Stack.Screen name="Detail" component={DetailScreen} />
+            {/* <Stack.Screen name='Create' component={CreateScreen} />
           <Stack.Screen name='Map' component={MapScreen} />
           <Stack.Screen name='Otherprofile' component={OtherprofileScreen} />
           <Stack.Screen name='Friend' component={ListFriend} />
           <Stack.Screen name='History' component={HistoryScreen} />
           <Stack.Screen name='Chat' component={ChatScreen} /> */}
-          {/* <Stack.Screen name='Resume' component={ResumeScreen} /> */}
-          <Stack.Screen name='HamburgerMenu' component={HamburgerMenuScreen} />
-          <Stack.Screen name='BottomMenuTabs' component={BottomMenuTabs} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
-  )
+            {/* <Stack.Screen name='Resume' component={ResumeScreen} /> */}
+            <Stack.Screen name="HamburgerMenu" component={HamburgerMenuScreen} />
+            <Stack.Screen name="BottomMenuTabs" component={BottomMenuTabs} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
   shadow: {
-    shadowColor: '#000000',
+    shadowColor: "#000000",
     shadowOffset: {
       width: 0,
       height: 10,
     },
     shadowOpacity: 1,
     shadowRadius: 3.5,
-    elevation: 5,
-  },
+    elevation: 5
+  }
 })
--- frontend/screens/Search.js
+++ frontend/screens/Search.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-import {Platform} from 'react-native'
+import MapView from 'react-native-maps'
+import {StyleSheet, FlatList} from 'react-native'
 
 import DateTimePickerModal from 'react-native-modal-datetime-picker'
 
@@ -12,6 +13,7 @@ import {
   Select,
   Button,
   CheckIcon,
+  ScrollView,
 } from 'native-base'
 
 function Search() {
@@ -19,6 +21,8 @@ function Search() {
   const [date, setDate] = useState()
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false)
   const [isHourPickerVisible, setHourPickerVisibility] = useState(false)
+  const [citie, setCitie] = useState()
+  const [listCities, setListCities] = useState([])
 
   const showDatePicker = () => {
     setDatePickerVisibility(true)
@@ -29,89 +33,145 @@ function Search() {
     setHourPickerVisibility(false)
   }
 
+  const searchCities = async (e) => {
+    setCitie(e)
+    var result = await fetch(`https://geo.api.gouv.fr/communes?nom=${e}`)
+    var response = await result.json()
+    var listCities = response.map((e) => e.nom)
+    setListCities([...listCities])
+  }
+
+  var listCitiesDisplay = (
+    <FlatList data={listCities} renderItem={({item}) => <Text>{item}</Text>} />
+  )
+
+  console.log(listCities)
+
   return (
     <Box
       style={{
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+        marginTop: 25,
       }}>
-      <Text h1 fontFamily='Roboto' fontSize={20}>
-        Chercher une randonnée
-      </Text>
-      <Input placeholder='Ville / département / région'></Input>
-      <HStack alignItems='center' space={4}>
-        <Text>Rando mixte</Text>
-        <Switch size='sm' />
-      </HStack>
-      <Input placeholder="Age de l'organisateur"></Input>
+      <Box
+        style={{
+          flex: 1,
+          width: '90%',
 
-      {/* sélection de la date */}
+          // justifyContent: 'center',
 
-      <Button
-        variant='outline'
-        mt='1.5'
-        w='100%'
-        colorScheme='secondary'
-        onPress={showDatePicker}>
-        <Text color='grey'>
-          {!date
-            ? 'Date & Heure'
-            : date.toLocaleDateString('fr') +
-              ' ' +
-              date.getHours() +
-              ':' +
-              date.getMinutes()}
+          alignItems: 'center',
+        }}>
+        <Text h1 fontFamily='Roboto' fontSize={20}>
+          Chercher une randonnée
         </Text>
-      </Button>
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode='date'
-        date={date}
-        onConfirm={(date) => {
-          setDatePickerVisibility(false)
-          setDate(date)
-          setHourPickerVisibility(true)
-        }}
-        onCancel={hidePicker}
-      />
 
-      <DateTimePickerModal
-        isVisible={isHourPickerVisible}
-        mode='time'
-        locale='fr-FR'
-        date={date}
-        onConfirm={(date) => {
-          setHourPickerVisibility(false)
-          setDate(date)
-        }}
-        onCancel={hidePicker}
-      />
+        <Input
+          style={styles.allInput}
+          placeholder='Ville / département / région'
+          onChangeText={(e) => searchCities(e)}
+          value={citie}></Input>
+        {listCitiesDisplay}
 
-      <Select
-        selectedValue={level}
-        w='100%'
-        accessibilityLabel='Niveau'
-        placeholder='Niveau'
-        _selectedItem={{
-          endIcon: <CheckIcon size='5' />,
-        }}
-        mt='1.5'
-        onValueChange={(itemValue) => setLevel(itemValue)}>
-        <Select.Item label='Facile' value='facile' />
-        <Select.Item label='Intermédiaire' value='intermediaire' />
-        <Select.Item label='Difficile' value='difficile' />
-      </Select>
+        <HStack alignItems='center' space={4}>
+          <Text>Rando mixte</Text>
+          <Switch size='sm' />
+        </HStack>
+        <Input
+          style={styles.allInput}
+          placeholder="Age de l'organisateur"></Input>
 
-      <Button mt='1.5' w='100%' bg='#78E08F'>
-        Rechercher
-      </Button>
+        {/* sélection de la date */}
 
-      <Box w='250' h='200' bg='#BBBBBB'>
-        Map
+        <Button
+          style={styles.allInput}
+          variant='outline'
+          mt='2'
+          w='100%'
+          colorScheme='secondary'
+          onPress={showDatePicker}>
+          <Text color='grey'>
+            {!date
+              ? 'Date & Heure'
+              : date.toLocaleDateString('fr') +
+                ' ' +
+                date.getHours() +
+                ':' +
+                date.getMinutes()}
+          </Text>
+        </Button>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode='date'
+          date={date}
+          onConfirm={(date) => {
+            setDatePickerVisibility(false)
+            setDate(date)
+            setHourPickerVisibility(true)
+          }}
+          onCancel={hidePicker}
+        />
+
+        <DateTimePickerModal
+          isVisible={isHourPickerVisible}
+          mode='time'
+          locale='fr-FR'
+          date={date}
+          onConfirm={(date) => {
+            setHourPickerVisibility(false)
+            setDate(date)
+          }}
+          onCancel={hidePicker}
+        />
+
+        <Select
+          selectedValue={level}
+          w='100%'
+          accessibilityLabel='Niveau'
+          placeholder='Niveau'
+          _selectedItem={{
+            endIcon: <CheckIcon size='5' />,
+          }}
+          mt='1.5'
+          onValueChange={(itemValue) => setLevel(itemValue)}>
+          <Select.Item label='Facile' value='facile' />
+          <Select.Item label='Intermédiaire' value='intermediaire' />
+          <Select.Item label='Difficile' value='difficile' />
+        </Select>
+
+        <Button mt='2' w='100%' bg='#78E08F'>
+          Rechercher
+        </Button>
+        <MapView style={styles.map}></MapView>
       </Box>
     </Box>
   )
 }
+
+const styles = StyleSheet.create({
+  allInput: {
+    backgroundColor: '#EEEEEE',
+    borderWidth: 0.5,
+    //height: 10,
+    borderColor: '#CCCCCC',
+  },
+  map: {
+    width: '100%',
+    marginTop: 10,
+    height: 200,
+    borderWidth: 10,
+    borderColor: '#CCCCCC',
+  },
+  container: {
+    borderWidth: 1.5,
+    borderColor: '#CCCCCC',
+  },
+  libelle: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: 350,
+  },
+})
 
 export default Search

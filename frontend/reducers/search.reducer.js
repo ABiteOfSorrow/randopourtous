c317346f7f46d1886eb2cd2242133ccc74e9d34d
Emmanuel Cordelle <emm.cordelle@gmail.com>
2022-05-19 23:44:09 +0200

    ajout popup sur la carte

--- frontend/reducers/search.reducer.js
+++ frontend/reducers/search.reducer.js
@@ -1,6 +1,6 @@
 export default function searchRando(searchData = {}, action) {
   if (action.type === 'addData') {
-    console.log('reducer: ', action.dataAdd)
+    // console.log('reducer: ', action.dataAdd)
     return action.dataAdd
   } else {
     return searchData

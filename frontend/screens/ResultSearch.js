3d2e7b4d45c3bf1bf6e34800b689b7da2b48606c
Emmanuel Cordelle <emm.cordelle@gmail.com>
2022-05-19 11:15:40 +0200

    dynamisation de ResultSearch

--- frontend/screens/ResultSearch.js
+++ frontend/screens/ResultSearch.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {
   HStack,
   VStack,
@@ -18,10 +18,15 @@ import backendConfig from '../backend.config.json'
 const backendAdress = backendConfig.address
 
 function ResultSearch(props) {
+  //****** state de récupération des résultats de recherche */
+  const [resultSearch, setResultSearch] = useState([])
+
+  //*** state de l'affichage de la carte (changement d'état avec le switch) */
+  const [mapdisplay, setMapDisplay] = false
+
+  //**** iniatilisation de la liste des résultat de recherche via requête dans la BDD */
   useEffect(() => {
     var searchFunction = async function () {
-      console.log('données envoyées ', props.data)
-
       let result = await fetch(backendAdress + '/search-track', {
         method: 'POST',
         headers: {
@@ -31,16 +36,63 @@ function ResultSearch(props) {
       })
 
       let response = await result.json()
-
       console.log('données recues: ', response.result)
-      // if (!data.result) {
-      //   Alert.alert("Erreur", data.error);
-      //   return;
-      // }
+      setResultSearch([...response.result])
     }
 
     searchFunction()
   }, [])
+
+  let listRando
+  //**** si pas de résultats, affichage d'une message d'erreur, sinon affichage de la liste des randos */
+  if (resultSearch.length === 0) {
+    listRando = (
+      <Heading size='lg' mb={5} textAlign='center'>
+        pas de Resultat
+      </Heading>
+    )
+  } else {
+    listRando = resultSearch.map((item, i) => (
+      <VStack key={i} space={2} alignItems='center'>
+        <Box w={'75%'} mb={0} borderRadius='15' bg='#78E08F'>
+          <Heading size='md' textAlign='center'>
+            {item.name}
+          </Heading>
+        </Box>
+        <Center
+          w={'80%'}
+          h={62}
+          p={0}
+          mb={2}
+          bg='#FAFAFA'
+          rounded='lg'
+          shadow={8}
+          display='flex'
+          flexDirection='row'
+          justifyContent='space-around'>
+          <Heading size='md'>{item.departure.nom}</Heading>
+
+          <VStack space={2} alignItems='flex-start'>
+            <Text fontSize='sm' bold>
+              {item.level}
+            </Text>
+            <Text fontSize='sm' bold>
+              5 / 12 particpants
+            </Text>
+          </VStack>
+          <Button
+            size='md'
+            backgroundColor='#78E08F'
+            alignSelf='center'
+            onPress={() => props.navigation.navigate('Detail', {item})}>
+            <Text style={styles.contentText} fontSize='md'>
+              Voir
+            </Text>
+          </Button>
+        </Center>
+      </VStack>
+    ))
+  }
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
@@ -68,49 +120,22 @@ function ResultSearch(props) {
           </Heading>
           {/* Switch Line */}
           <Box display='flex' flexDirection='row' alignItems='center' mb={5}>
-            <Switch offTrackColor='#C4C4C4' onTrackColor='#78E08F' mr={4} />
+            <Switch
+              offTrackColor='#C4C4C4'
+              onTrackColor='#78E08F'
+              mr={4}
+              onValueChange={() => {
+                setMixte(!mixte)
+                console.log(mixte)
+              }}
+            />
             <Heading size='md'>Vue carte</Heading>
           </Box>
         </VStack>
-        <VStack space={2} alignItems='center'>
-          {/* Journey List */}
-          <Box w={'75%'} mb={0} borderRadius='15' bg='#78E08F'>
-            <Heading size='md' textAlign='center'>
-              Totos's Rando pour tous
-            </Heading>
-          </Box>
-          <Center
-            w={'80%'}
-            h={62}
-            p={0}
-            mb={2}
-            bg='#FAFAFA'
-            rounded='lg'
-            shadow={8}
-            display='flex'
-            flexDirection='row'
-            justifyContent='space-around'>
-            <Heading size='md'>Vincennes</Heading>
 
-            <VStack space={2} alignItems='flex-start'>
-              <Text fontSize='sm' bold>
-                Sportif
-              </Text>
-              <Text fontSize='sm' bold>
-                5 / 12 particpants
-              </Text>
-            </VStack>
-            <Button
-              size='md'
-              backgroundColor='#78E08F'
-              alignSelf='center'
-              onPress={() => console.log("I'm Pressed")}>
-              <Text style={styles.contentText} fontSize='md'>
-                Voir
-              </Text>
-            </Button>
-          </Center>
-        </VStack>
+        {/* Journey List */}
+
+        {listRando}
       </ScrollView>
       {/* To prevent leaving the content area */}
       <Box w='100%' h='8.5%' alignSelf='center' bg='#fff' />

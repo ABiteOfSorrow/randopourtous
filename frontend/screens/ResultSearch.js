b42211e474445d3497c836341ca242e3e1248490
ABiteOfSorrow <laceican@naver.com>
2022-05-20 15:55:52 +0200

    Fix design for ResultSearch (Map)

--- frontend/screens/ResultSearch.js
+++ frontend/screens/ResultSearch.js
@@ -238,10 +238,11 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
-    width: 350,
+    width: "100%",
     height: '100%',
     borderWidth: 10,
     borderColor: '#CCCCCC',
+    alignSelf: "center"
   },
   mapContainer: {
     flex: 1,

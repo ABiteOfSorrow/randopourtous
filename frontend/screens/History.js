838d4fef44fec38c2636cd02029eaa860948bd70
OliverPtps <oliver.printemps@yahoo.fr>
2022-05-25 16:13:30 +0200

    rein de spécial

--- frontend/screens/History.js
+++ frontend/screens/History.js
@@ -118,8 +118,8 @@ function History(props) {
 
   //Affichage
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <ScrollView h="80%">
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff", heigth:"100%" }}>
+      <ScrollView>
         <HStack style={{ justifyContent: "space-between" }} mb={4}>
           <HamburgerMenu navigation={props.navigation} />
           <Button w={90} h={8} p={0} mt={2} mr={2} variant="outline" style={{ borderColor: "#38ADA9" }} onPress={() => props.navigation.goBack()}>

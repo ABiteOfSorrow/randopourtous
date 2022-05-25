14be70c52e9ede4711fb5e5af887fddb382f68fd
ABiteOfSorrow <laceican@naver.com>
2022-05-25 17:02:22 +0200

    Add console Hello

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -263,18 +263,18 @@ router.get('/search-user-track', async (req, res) => {
 
 });
 
-router.post('/finish-track', async (req, res) => {
-  console.log(ok)
-  // if (!req.query.id) {
-  //   return res.json({ result: false, error: 'Token manquant svp' })
-  // }
-  // let foundRando = await randoModel.findById(req.query.id);
-  // if (!foundRando) {
-  //   return res.json({ result: false, error: 'Rando pas trouvé svp' })
-  // }
-  return res.json({ result: true, track: foundRando })
+// router.post('/finish-track', async (req, res) => {
+//   console.log("ok")
+//   // if (!req.query.id) {
+//   //   return res.json({ result: false, error: 'Token manquant svp' })
+//   // }
+//   // let foundRando = await randoModel.findById(req.query.id);
+//   // if (!foundRando) {
+//   //   return res.json({ result: false, error: 'Rando pas trouvé svp' })
+//   // }
+//   return res.json({ result: true, track: foundRando })
 
-});
+// });
 
 
 router.post('/update-randorating', async (req, res) => {

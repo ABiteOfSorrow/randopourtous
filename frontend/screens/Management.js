244b69bc20febed10669a0b778426104f677a5ce
Emmanuel Cordelle <emm.cordelle@gmail.com>
2022-05-26 15:24:40 +0200

    modif du fake dans management screen

--- frontend/screens/Management.js
+++ frontend/screens/Management.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Avatar, HStack, VStack, Center, Heading, Box, Button, Text, Flex, Stack } from "native-base";
 import { StyleSheet, ScrollView, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -12,6 +12,9 @@ const backendAdress = backendConfig.address
 
 function Management(props) {
   console.log("props.rando",props.route.params.params.rando.users.length)
+  
+  //******* Statu d'acceptation pour faker */
+  const [acceptStatus, setAcceptStatus]=useState(false)
 
   async function handleSubmit() {
 
@@ -60,8 +63,9 @@ function Management(props) {
         <VStack space={2} mb={20} alignItems="center">
           <Heading size="lg">Demandes de partipation </Heading>
           {/* User profil box */}
+          {acceptStatus===false? 
           <Box w={"90%"} h={110} bg={"#bbbbbb"} borderRadius={15} p={0} shadow="9" justifyContent={"center"} alignItems={"center"}>
-            <Center w={"95%"} h={62} p={0} mb={2} bg="#079992" rounded="lg" shadow={8} display="flex" flexDirection="row" justifyContent="space-around">
+             <Center w={"95%"} h={62} p={0} mb={2} bg="#079992" rounded="lg" shadow={8} display="flex" flexDirection="row" justifyContent="space-around">
               <Avatar
                 me="10"
                 bg="amber.500"
@@ -87,9 +91,11 @@ function Management(props) {
                 </Text>
               </Button>
             </Center>
+            
+
             {/* Accepter & Refuser buttons */}
             <HStack>
-              <Button w={"30%"} h={25} p={0} mr={10} borderRadius={15} bg={"#079992"} shadow="9" >
+              <Button w={"30%"} h={25} p={0} mr={10} borderRadius={15} bg={"#079992"} shadow="9" onPress={()=>  setAcceptStatus(true)} >
                 {" "}
                 Accepter{" "}
               </Button>
@@ -99,13 +105,56 @@ function Management(props) {
               </Button>
             </HStack>
           </Box>
+          :<Text></Text>}
         </VStack>
 
         {/* contents container for Liste des participants */}
         <VStack space={2} mb={2} alignItems="center">
           <Heading size="lg">Liste des participants </Heading>
           {/* User profil box */}
-          <Box w={"90%"} h={110} bg={"#bbbbbb"} borderRadius={15} p={0} shadow="9" justifyContent={"center"} alignItems={"center"}>
+          
+            {acceptStatus===true?          
+                    <Box w={"90%"} h={110} bg={"#bbbbbb"} borderRadius={15} p={0} shadow="9" justifyContent={"center"} alignItems={"center"}>
+                    <Center w={"95%"} h={62} p={0} mb={2} bg="#079992" rounded="lg" shadow={8} display="flex" flexDirection="row" justifyContent="space-around">
+                     <Avatar
+                       me="10"
+                       bg="amber.500"
+                       source={{
+                         uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+                       }}
+                     ></Avatar>
+                     <VStack space={2} alignItems="flex-start">
+                       <Heading style={styles.contentText} size="xs">
+                         Toto
+                       </Heading>
+                       <Flex direction="row" alignSelf="center">
+                         <AntDesign name="star" size={24} color="yellow" />
+                         <AntDesign name="star" size={24} color="yellow" />
+                         <AntDesign name="star" size={24} color="yellow" />
+                         <AntDesign name="star" size={24} color="yellow" />
+                         <AntDesign name="star" size={24} color="yellow" />
+                       </Flex>
+                     </VStack>
+                     <Button size="xs" backgroundColor="#BBBBBB" alignSelf="center" shadow="9" onPress={() => console.log("I'm Pressed")}>
+                       <Text style={styles.contentText} fontSize="xs">
+                         Voir Profil
+                       </Text>
+                     </Button>
+                   </Center>
+                   
+       
+                   {/* Accepter & Refuser buttons */}
+                   <HStack>
+           {/* Ban buttons */}
+           <Button w={"30%"} h={25} p={0} borderRadius={15} bg={"#E55039"} shadow="9" >
+              {" "}
+              Ban{" "}
+            </Button>
+                   </HStack>
+                 </Box>
+            :<Text></Text>}
+
+            <Box w={"90%"} h={110} bg={"#bbbbbb"} borderRadius={15} p={0} shadow="9" justifyContent={"center"} alignItems={"center"}>
             <Center w={"95%"} h={62} p={0} mb={2} bg="#079992" rounded="lg" shadow={8} display="flex" flexDirection="row" justifyContent="space-around">
               <Avatar
                 me="10"
